8a779c194568db4dd3914687485ffd6d16a83dcc
Satar Emami <satar@artemis.com>
2022-12-16 13:55:22 +0330

    update :)

--- src/component/chatBox/mesasge/ChatMessage.tsx
+++ src/component/chatBox/mesasge/ChatMessage.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import * as Material from "@mui/material";
 import * as MaterialIcons from "@mui/icons-material"
-import {useEffect, useState} from "react";
-
+import Lottie from "lottie-react"
+import AnimateLottie from "../../../asset/lottieflow-loading.json"
 export default function ChatMessage(props: ChatMessageProps) {
-    useEffect(() => {
+    const [state,setstate]=React.useState({tryToConnect:false})
+    React.useEffect(() => {
         if (!props.webSocket)return;
         props.webSocket.onopen=(ev:Event)=>{
             props.setConnectionStatus(true)
@@ -14,29 +15,16 @@ export default function ChatMessage(props: ChatMessageProps) {
         }
     },[props.webSocket])
     const openWebSocket=()=>{
+        state.tryToConnect=true
+        setstate({...state})
         props.setWebSocket(new WebSocket("wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self"))
     }
     return (
         <div style={{width: "100%", height: "100%", backgroundColor: "#ffe3e3"}}>
-            {!props.connectionStatus ?
-                <div style={{height: "100%", width: "100%"}}>
-                    <div style={{
-                        display: "flex",
-                        alignItems: "end",
-                        justifyContent: "center",
-                        height: "55%",
-                        width: "100%"
-                    }}>
-                        <Material.IconButton color="primary" onClick={openWebSocket}>
-                            <MaterialIcons.SupportAgent style={{width: 80, height: 80}}/>
-                        </Material.IconButton>
-                    </div>
-                    <div style={{display: "flex", justifyContent: "center", height: "45%", width: "100%"}}>
-                        Connect To Admin
-                    </div>
-                </div>
+            {!state.tryToConnect ?
+                <UiTryToConnect openWebSocket={openWebSocket}/>
                 :
-                ""
+                props.connectionStatus? <ChatObjectUi/>:<TryToConnect/>
             }
         </div>
     )
@@ -46,4 +34,46 @@ interface ChatMessageProps {
     setWebSocket:(webSocket:WebSocket|null)=>void
     connectionStatus:boolean
     setConnectionStatus:(status:boolean) =>void
+}
+function UiTryToConnect(props: any){
+    return(
+        <div style={{height: "100%", width: "100%"}}>
+            <div style={{
+                display: "flex",
+                alignItems: "end",
+                justifyContent: "center",
+                height: "55%",
+                width: "100%"
+            }}>
+                <Material.IconButton color="primary" onClick={props.openWebSocket}>
+                    <MaterialIcons.SupportAgent style={{width: 80, height: 80}}/>
+                </Material.IconButton>
+            </div>
+            <div style={{display: "flex", justifyContent: "center", height: "45%", width: "100%"}}>
+                Connect To Admin
+            </div>
+        </div>
+    )
+}
+function TryToConnect(){
+    return(
+        <div style={{height: "100%", width: "100%"}}>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+                width: "100%"
+            }}>
+                <Lottie style={{width:"50%",height:"50%"}} animationData={AnimateLottie} loop={true}/>
+            </div>
+        </div>
+    )
+}
+function ChatObjectUi(){
+    return(
+        <div>
+            Chat Object Ui
+        </div>
+    )
 }
